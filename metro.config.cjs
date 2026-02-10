899db2e17313576fda0a24cbd69447c85412aaa5
MwiineIrwin <mwiineirwin23@gmail.com>
2026-02-10 17:45:26 +0300

    02

--- metro.config.cjs
+++
@@ -1,7 +0,0 @@
-const { getDefaultConfig } = require('@expo/metro-config');
-
-const defaultConfig = getDefaultConfig(__dirname);
-
-defaultConfig.resolver.assetExts.push('tflite');
-
-module.exports = defaultConfig;
